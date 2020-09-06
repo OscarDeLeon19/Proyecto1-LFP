2e14ba5b72c827c7f4cd5d0bdcef1cf6e303b200
OscarDeLeon19 <oscar99deleon19@gmail.com>
2020-09-05 22:16:31 -0600

    Agregamos las palabras reservadas de cadena, caracter, decimal, entero y commentarios por //

--- Proyecto1-LabLFP/Analizador.cs
+++ Proyecto1-LabLFP/Analizador.cs
@@ -12,6 +12,7 @@ namespace Proyecto1_LabLFP
     class Analizador
     {
         private string[] palabras = new string[100];
+        private int CantidadPalabras;
         private int[] espacios = new int[100];
         public Analizador()
         {
@@ -20,31 +21,106 @@ namespace Proyecto1_LabLFP
 
         public void Analizar(RichTextBox Area, String LineaDeTexto, int Posicion, int NumLinea)
         {
-            LineaDeTexto = LineaDeTexto + " ";
-            int NoEspacio = 0;
-            char[] caracter = LineaDeTexto.ToCharArray();
-            for (int i = 0; i < caracter.Length; i++)
+            if (LineaDeTexto.StartsWith("//"))
             {
-                if (Char.IsWhiteSpace(caracter[i]))
+                PintarPalabra(Posicion, 0, LineaDeTexto.Length, LineaDeTexto, Area, NumLinea);
+            }
+            else
+            {
+                CantidadPalabras = 0;
+                LineaDeTexto = LineaDeTexto + " ";
+                char[] caracter = LineaDeTexto.ToCharArray();
+                for (int i = 0; i < caracter.Length; i++)
                 {
-                    if (NoEspacio == 0)
+                    string mini = caracter[i].ToString();
+                    if (Char.IsLetter(caracter[i]))
                     {
-                        palabras[NoEspacio] = LineaDeTexto.Substring(0, i);
+                        string aux = "";
+                        for (int j = i; j < caracter.Length; j++)
+                        {
+
+                            if (Char.IsLetter(caracter[j]) || Char.IsNumber(caracter[j]))
+                            {
+                                aux = aux + caracter[j].ToString();
+                            }
+                            else if (Char.IsWhiteSpace(caracter[j]))
+                            {
+
+                                PintarPalabra(Posicion, i, j - i, aux, Area, NumLinea);
+                                i = j;
+                                j = caracter.Length;
+                                palabras[CantidadPalabras] = aux;
+                                CantidadPalabras++;
+                            }
+                        }
                     }
-                    else
-                    {
-                        palabras[NoEspacio] = LineaDeTexto.Substring(espacios[NoEspacio -1]+1, i - espacios[NoEspacio - 1]);
-                    }
-                    espacios[NoEspacio] = i;
-                    NoEspacio++;
                 }
             }
-            NoEspacio++;
-            MessageBox.Show("Palabras: " + NoEspacio.ToString());
-            for (int i = 0; i < NoEspacio - 1; i++)
+        }
+
+        public void PintarPalabra(int position, int inicio, int final, string palabra, RichTextBox Area, int linea)
+        {
+            if (palabra.StartsWith("//"))
             {
-                MessageBox.Show(palabras[i]);
+                int pos = ObtenerPosicion(Area, linea, inicio);
+                Area.Select(pos, final);
+                Area.SelectionColor = Color.Red;
+                Area.SelectionStart = position;
             }
+            else if (palabra == "entero")
+            {
+                int pos = ObtenerPosicion(Area, linea, inicio);
+                Area.Select(pos, final);
+                Area.SelectionColor = Color.Magenta;
+                Area.SelectionStart = position;
+            }
+            else if (palabra == "decimal")
+            {
+                int pos = ObtenerPosicion(Area, linea, inicio);
+                Area.Select(pos, final);
+                Area.SelectionColor = Color.Aquamarine;
+                Area.SelectionStart = position;
+            }
+            else if (palabra == "cadena")
+            {
+                int pos = ObtenerPosicion(Area, linea, inicio);
+                Area.Select(pos, final);
+                Area.SelectionColor = Color.Gray;
+                Area.SelectionStart = position;
+            }
+            else if (palabra == "booleano")
+            {
+                int pos = ObtenerPosicion(Area, linea, inicio);
+                Area.Select(pos, final);
+                Area.SelectionColor = Color.Orange;
+                Area.SelectionStart = position;
+            }
+            else if (palabra == "caracter")
+            {
+                int pos = ObtenerPosicion(Area, linea, inicio);
+                Area.Select(pos, final);
+                Area.SelectionColor = Color.Brown;
+                Area.SelectionStart = position;
+            }
+            else
+            {
+                int pos = ObtenerPosicion(Area, linea, inicio);
+                Area.Select(pos, final);
+                Area.SelectionColor = Color.Black;
+                Area.SelectionStart = position;
+            }
+        }
+
+        public int ObtenerPosicion(RichTextBox Area, int linea, int inicio)
+        {
+            int pos = 0;
+            for (int j = 0; j < linea; j++)
+            {
+                pos = pos + Area.Lines[j].Length;
+            }
+            pos = pos + (linea + 1);
+            pos = pos + (inicio - 1);
+            return pos;
         }
     }
 }
